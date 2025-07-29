89a7f80aef9a654fd74ae2dac9e75c221f578f04
jpgaertner <jph.gaertner@gmail.com>
2025-07-29 18:01:07 +0200

    add fill_circular_overlap

--- veris/fill_overlap.py
+++ veris/fill_overlap.py
@@ -1,17 +1,31 @@
 import jax
+import jax.numpy as jnp
 import jaxdecomp
+from functools import partial
 
 @jax.jit
-def fill_overlap(var):
-    return jaxdecomp.halo_exchange(
-            var[:,:,jnp.newaxis], # the jaxdecomp.halo_exchange only works on 3D arrays
-            halo_extents=(2, 2), # total halo size in each dimension
-            halo_periods=(True, True)
-            # True -> periodic/ cyclic halo exchange, the halo values at the left edge
-            # of one partition are exchanged with the halo values at the right edge of
-            # the adjacent partition. this can be visualized as overlap between partitionings
-        )[:,:,0] # remove third axis
+def fill_circular_overlap(A):
+        A = A.at[:2, :].set(A[-4:-2, :])
+        A = A.at[-2:, :].set(A[2:4, :])
+        A = A.at[:, :2].set(A[:, -4:-2])
+        A = A.at[:, -2:].set(A[:, 2:4])
 
-@jax.jit
-def fill_overlap_uv(u, v):
-    return fill_overlap(u), fill_overlap(v)
+        return A
+
+@partial(jax.jit, static_argnames=['sett'])
+def fill_overlap(sett, var):
+    if sett.use_circular_overlap:
+        return fill_circular_overlap(var)
+    else:
+        return jaxdecomp.halo_exchange(
+                var[:,:,jnp.newaxis], # the jaxdecomp.halo_exchange only works on 3D arrays
+                halo_extents=(2, 2), # total halo size in each dimension
+                halo_periods=(True, True)
+                # True -> periodic/ cyclic halo exchange, the halo values at the left edge
+                # of one partition are exchanged with the halo values at the right edge of
+                # the adjacent partition. this can be visualized as overlap between partitionings
+            )[:,:,0] # remove third axis
+
+@partial(jax.jit, static_argnames=['sett'])
+def fill_overlap_uv(sett, u, v):
+    return fill_overlap(sett, u), fill_overlap(sett, v)
