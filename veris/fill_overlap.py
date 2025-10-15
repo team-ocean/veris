c3676fbff45da8001799e2861f07ca1e6787f82a
jpgaertner <jph.gaertner@gmail.com>
2025-10-15 14:34:06 +0200

    use with_sharding_constraint, needed for PE grids with only dim equal 1

--- veris/fill_overlap.py
+++ veris/fill_overlap.py
@@ -18,14 +18,23 @@ def fill_overlap(sett, var):
     if sett.use_circular_overlap:
         return fill_circular_overlap(var)
     else:
-        return jaxdecomp.halo_exchange(
-                var[:,:,jnp.newaxis], # the jaxdecomp.halo_exchange only works on 3D arrays
-                halo_extents=(2, 2), # total halo size in each dimension
-                halo_periods=(True, True)
+        # the jaxdecomp.halo_exchange only works on 3D arrays
+        var = var[:,:,jnp.newaxis]
+
+        # force the compiler to keep the sharding for SPMD lowering
+        # (this is needed for when only one of the processor grid axes is dim 1)
+        var = jax.lax.with_sharding_constraint(var, sett.sharding)
+
+        out = jaxdecomp.halo_exchange(
+            var,
+            halo_extents=(2, 2), # total halo size in each dimension
+            halo_periods=(True, True)
                 # True -> periodic/ cyclic halo exchange, the halo values at the left edge
                 # of one partition are exchanged with the halo values at the right edge of
                 # the adjacent partition. this can be visualized as overlap between partitionings
-            )[:,:,0] # remove third axis
+            )
+
+        return out[:,:,0] # remove third axis
 
 @partial(jax.jit, static_argnames=['sett'])
 def fill_overlap_uv(sett, u, v):
